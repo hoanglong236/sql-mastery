9856deccbc805ebfdc94bebb58c4560e5769e20e
hoanglong236 <hahahalalala236@gmail.com>
2026-07-16 06:47:25 +0700

    feat(hackerrank/sql): add Weather Observation Station 7 solution (s0)

---
+++ practice/hackerrank/sql/weather-observation-station-7/weather-observation-station-7_s0.sql
@@ -0,0 +1,17 @@
+SET NULL "NULL";
+SET FEEDBACK OFF;
+SET ECHO OFF;
+SET HEADING OFF;
+SET WRAP OFF;
+SET LINESIZE 10000;
+SET TAB OFF;
+SET PAGES 0;
+SET DEFINE OFF;
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(SUBSTR(CITY, -1, 1)) IN ('a', 'e', 'i', 'o', 'u');
+
+exit;
