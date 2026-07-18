4274ddfae2ba0dfdb5bb72f94088fc7c03f14757
hoanglong236 <hahahalalala236@gmail.com>
2026-07-18 14:42:12 +0700

    feat(hackerrank/sql): add Higher Than 75 Marks solution (s0)

---
+++ practice/hackerrank/sql/higher-than-75-marks/higher-than-75-marks_s0.sql
@@ -0,0 +1,17 @@
+SET NULL "NULL";
+SET FEEDBACK OFF;
+SET ECHO OFF;
+SET HEADING OFF;
+SET WRAP OFF;
+SET LINESIZE 10000;
+SET TAB OFF;
+SET PAGES 0;
+SET DEFINE OFF;
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY SUBSTR(NAME, LENGTH(NAME) - 2, 3), ID;
+
+exit;
